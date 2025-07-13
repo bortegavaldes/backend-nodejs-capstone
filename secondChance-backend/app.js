d03fa5be85742ed277de61275db7e86563c6103a
bortegavaldes <bortegavaldes@gmail.com>
2025-07-12 23:20:56 -0400

    Implementation of search api

--- secondChance-backend/app.js
+++ secondChance-backend/app.js
@@ -25,12 +25,9 @@ app.use(express.json());
 
 // authRoutes Step 2: import the authRoutes and store in a constant called authRoutes
 
-
-// Items API Task 1: import the secondChanceItemsRoutes and store in a constant called secondChanceItemsRoutes
 const secondChanceItemsRoutes = require('./routes/secondChanceItemsRoutes');
 
-// Search API Task 1: import the searchRoutes and store in a constant called searchRoutes
-//{{insert code here}}
+const searchRoutes = require('./routes/searchRoutes');
 
 
 const pinoHttp = require('pino-http');
@@ -42,11 +39,9 @@ app.use(pinoHttp({ logger }));
 // authRoutes Step 2: add the authRoutes and to the server by using the app.use() method.
 //{{insert code here}}
 
-// Items API Task 2: add the secondChanceItemsRoutes to the server by using the app.use() method.
 app.use('/api/secondchance/items', secondChanceItemsRoutes);
 
-// Search API Task 2: add the searchRoutes to the server by using the app.use() method.
-//{{insert code here}}
+app.use('/api/secondchance/search', searchRoutes);
 
 
 // Global Error Handler
