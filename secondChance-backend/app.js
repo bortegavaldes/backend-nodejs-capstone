2fe0f1b476a108e9ff02fd6217ff7952ae5dafbf
bortegavaldes <bortegavaldes@gmail.com>
2025-07-19 22:07:13 -0300

    Update app.js for linting action

--- secondChance-backend/app.js
+++ secondChance-backend/app.js
@@ -1,12 +1,9 @@
-/*jshint esversion: 8 */
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const pinoLogger = require('./logger');
 
 const connectToDatabase = require('./models/db');
-const {loadData} = require("./util/import-mongo/index");
-
 
 const app = express();
 app.use("*",cors());
@@ -18,17 +15,12 @@ connectToDatabase().then(() => {
 })
     .catch((e) => console.error('Failed to connect to DB', e));
 
-
 app.use(express.json());
 
 // Route files
-
 const authRoutes = require('./routes/authRoutes');
-
 const secondChanceItemsRoutes = require('./routes/secondChanceItemsRoutes');
-
 const searchRoutes = require('./routes/searchRoutes');
-
 
 const pinoHttp = require('pino-http');
 const logger = require('./logger');
@@ -37,11 +29,8 @@ app.use(pinoHttp({ logger }));
 
 // Use Routes
 app.use('/api/auth', authRoutes);
-
 app.use('/api/secondchance/items', secondChanceItemsRoutes);
-
 app.use('/api/secondchance/search', searchRoutes);
-
 
 // Global Error Handler
 app.use((err, req, res, next) => {
